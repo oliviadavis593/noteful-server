5bcd307b756e947c6986e4127cd4d79989d64cbc
Olivia Davis <olivia.davis593@gmail.com>
2020-04-11 20:16:20 -0500

    migrations & seeding

---
+++ seeds/seed.noteful_folders.sql
@@ -0,0 +1,6 @@
+INSERT INTO noteful_folders ( folder_name )
+VALUES  
+    ('folderOne'),
+    ('folderTwo'),
+    ('folderThree'),
+    ('folderFour');
---
+++ seeds/seed.noteful_notes.sql
@@ -0,0 +1,6 @@
+INSERT INTO noteful_notes ( note_name, content, folder_id )
+VALUES 
+    ('Super duper duties', 'lorem ispum', 1),
+    ('Solar power', 'energy', 2),
+    ('Resistance', 'rope', 3),
+    ('Lord', 'power', 4)
